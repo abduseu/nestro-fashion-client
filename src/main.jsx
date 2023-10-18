689f95a6924a67f1f78ad3a094994e67a18b4d81
sal400 <flarebd@gmail.com>
2023-10-18 10:32:12 +0600

    Initial setup done for the client side

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./components/Root";
+import HomePage from "./pages/HomePage";
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root/>,
+    errorElement: <h2>Page Not Found!!!</h2>,
+    children: [
+      {
+        path: '/',
+        element: <HomePage/>,
+      },
+    ]
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
