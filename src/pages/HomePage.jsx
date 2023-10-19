f34cde66cf476206ff4e3714dffa7d4a0f2cc828
sal400 <flarebd@gmail.com>
2023-10-19 15:00:47 +0600

    brand logos added to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,14 +7,36 @@ const HomePage = () => {
         <div>
             <Banner></Banner>
 
-            <div className="bg-black text-white my-14 rounded-lg lg:flex lg:justify-between">
-                <div className="p-5 md:p-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full">
-                    <li>item-1</li>
-                    <li>item-2</li>
-                    <li>item-3</li>
-                    <li>item-4</li>
-                    <li>item-5</li>
-                    <li>item-6</li>
+            <div className="bg-black text-white my-14 rounded-lg text-center text-2xl font-semibold uppercase">
+                <div>
+                    <div className="p-5 gap-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full">
+
+                        <div>
+                            <img src="https://i.ibb.co/dPgVhGM/levis.png" alt="" />
+                            <h3>Levis</h3>
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/mGWYcVm/champion.png" alt="" />
+                            <h3>Champion</h3>
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/5M38sw4/nike.png" alt="" />
+                            <h3>Nike</h3>
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/BgxqfmF/calvin.png" alt="" />
+                            <h3>CK</h3>
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/Gsw3pJZ/dolce.png" alt="" />
+                            <h3>D & G</h3>
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/wyTFGKN/armani.png" alt="" />
+                            <h3>Armani</h3>
+                        </div>
+
+                    </div>
                 </div>
             </div>
 
